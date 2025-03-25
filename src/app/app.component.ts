2a4ceb2b4de3c9d2a68ff2226047aa979216da8c
AlanGaber11 <alangaber11@gmail.com>
2025-03-24 20:58:59 -0600

    Implementacion de HTTPS de manera local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-/* SWIPER js */
+/* SWIPER js (CARRUSEL) */
 import { register } from 'swiper/element/bundle';
 register();
 
@@ -7,7 +7,7 @@ register();
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'interface';
